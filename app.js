05a87c5f2535fbc3ed2ea3738b3aef385744c34b
FireMugen <maxxreeves@icloud.com>
2019-08-30 18:22:25 +1000

    changed view engine

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const ejs = require('ejs');
 const serverless = require('serverless-http');
+const cons = require('consolidate');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -12,9 +13,11 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 
+app.engine('html', cons.swig)
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+// app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
